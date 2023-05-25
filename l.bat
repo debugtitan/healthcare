2761d4fe100ca7974dcfb457b2a0b4c7334ff0b7
SolidityDevs <soliditydevs@gmail.com>
2023-05-25 21:52:41 +0100

    new updates forwarded

--- l.bat
+++ l.bat
@@ -1,4 +1,11 @@
 cls
+python manage.py makemigrations frontend
+python manage.py migrate frontend
+python manage.py makemigrations blogPage
+python manage.py migrate blogPage
+python manage.py makemigrations adminpage
+python manage.py migrate adminpage
+python manage.py migrate
 python manage.py collectstatic
 git add .
 git commit -m "new updates forwarded"
